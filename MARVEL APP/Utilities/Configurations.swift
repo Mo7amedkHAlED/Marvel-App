3469d240f53f00ab3c7360f787046441f965a678
Mohamed Khaled <mohammdk393@gmail.com>
2022-11-03 09:40:07 +0200

    handle character collection view if no data coming

---
+++ MARVEL APP/Utilities/Configurations.swift
@@ -0,0 +1,19 @@
+//
+//  Configurations.swift
+//  MARVEL APP
+//
+//  Created by Mohamed Khaled on 02/11/2022.
+//
+
+import Foundation
+
+struct Configurations {
+    static func getValue(for key: String) -> String{
+        guard let value = Bundle.main.object(forInfoDictionaryKey: key)  else
+        {
+            return " "
+            
+        }
+        return value as! String
+    }
+}
